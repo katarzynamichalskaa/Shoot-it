75d13822764c83d5f6083ee14d653a9912eb2a96
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-06-24 11:00:14 +0200

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5f; // prędkość poruszania się postaci
+    public float speed = 5f; 
 
-    Rigidbody2D rb; // komponent Rigidbody
-    float horizontalMovement; // wartość przesunięcia w poziomie
-    float verticalMovement; // wartość przesunięcia w pionie
+    Rigidbody2D rb; 
+    float horizontalMovement; 
+    float verticalMovement; 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // pobranie komponentu Rigidbody z obiektu
+        rb = GetComponent<Rigidbody2D>(); 
         if (rb == null)
         {
             Debug.LogError("Rigidbody component not found!");
@@ -21,28 +21,23 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        // pobranie wartości w osiach x i z z klawiszy A, D, W, S
+
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
 
-        // Obliczanie kąta obrotu na podstawie pozycji myszki
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePosition - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        // Obracanie postaci w kierunku myszki
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        // obliczenie wektora poruszania się postaci
         Vector3 movement = new Vector3(horizontalMovement, verticalMovement, 0f).normalized * speed * Time.fixedDeltaTime;
 
-        // poruszanie się postaci na skos
         if (horizontalMovement != 0f && verticalMovement != 0f)
         {
-            movement *= 0.7f; // zmniejszenie prędkości ruchu na skos
+            movement *= 0.7f; 
         }
 
-        // poruszanie się postaci za pomocą Rigidbody
         rb.MovePosition(transform.position + movement);
     }
 
