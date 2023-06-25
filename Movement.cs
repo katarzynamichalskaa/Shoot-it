39231fd5d324b0d5473b2b23863038b9ad4aff92
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-06-25 18:23:23 +0200

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -25,11 +25,20 @@ public class Movement : MonoBehaviour
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
 
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePosition - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (Time.timeScale == 1f)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 direction = mousePosition - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        else if (Time.timeScale == 0f)
+        {
+            Debug.LogError("Game is paused");
+        }
 
         Vector3 movement = new Vector3(horizontalMovement, verticalMovement, 0f).normalized * speed * Time.fixedDeltaTime;
 
