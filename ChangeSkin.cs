2c2ff4ff780cd0a50ef02ac3430ba69aa56f643c
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-07-02 15:53:15 +0200

    Create ChangeSkin.cs

---
+++ ChangeSkin.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkin : MonoBehaviour
+{
+    public Sprite sprite1;
+    public Sprite sprite2;
+    private SpriteRenderer spriteRenderer;
+    public bool SkinIsBought = false;
+    public int ValueOfSkin = 1;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite1;
+
+    }
+
+    void Update()
+    {
+        int myVariable = PlayerPrefs.GetInt("SkinIsBought", ValueOfSkin);
+        SkinIsBought = myVariable == ValueOfSkin;
+
+        if (myVariable == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                if (spriteRenderer.sprite == sprite1)
+                {
+                    spriteRenderer.sprite = sprite2;
+                }
+                else
+                {
+                    spriteRenderer.sprite = sprite1;
+                }
+            }
+        }
+    }
+
+    public void AddSkinWasBought()
+    {
+        SkinIsBought = true;
+
+        PlayerPrefs.SetInt("SkinIsBought", 1);
+
+        PlayerPrefs.Save();
+    }
+}
