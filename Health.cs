ac6afd1506cc9e63831919f621684adbe49dfe32
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-07-02 15:53:28 +0200

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -10,11 +10,16 @@ public class Health : MonoBehaviour
     private float collision_lasting = 0f;
     public float allowed_collision_lasting = 1f;
     private bool is_on_Ground;
+    private bool is_touching_enemy;
     public GameObject player;
     public GameObject heart1;
     public GameObject heart2;
     public GameObject heart3;
+    public GameObject heart4;
+    public bool AnotherHeartIsBought = false;
     public CountingToRespawn countingToRespawn;
+    public int ValueOfHeart = 1;
+
 
     void Start()
     {
@@ -22,7 +27,10 @@ public class Health : MonoBehaviour
         heart1.SetActive(true);
         heart2.SetActive(true);
         heart3.SetActive(true);
+        heart4.SetActive(false);
     }
+
+
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -39,6 +47,13 @@ public class Health : MonoBehaviour
             HealthDecrease();
         }
 
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            is_touching_enemy = true;
+            collision_lasting = Time.time;
+            HealthDecrease();
+        }
+
         if (collision.gameObject.CompareTag("Heal"))
         {
             Destroy(collision.gameObject);
@@ -52,52 +67,111 @@ public class Health : MonoBehaviour
         {
             is_on_Ground = false;
         }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            is_touching_enemy = false;
+        }
     }
 
     void HealthDecrease()
     {
         currentHealth = currentHealth - 1;
 
-        if (currentHealth == 2)
+        if (AnotherHeartIsBought == false)
         {
-            heart3.SetActive(false);
+            if (currentHealth == 2)
+            {
+                heart3.SetActive(false);
+            }
+            else if (currentHealth == 1)
+            {
+                heart2.SetActive(false);
+            }
+            else if (currentHealth == 0)
+            {
+                heart1.SetActive(false);
+                Respawn();
+            }
         }
-        else if (currentHealth == 1)
+
+        if(AnotherHeartIsBought)
         {
-            heart2.SetActive(false);
-        }
-        else if (currentHealth == 0)
-        {
-            heart1.SetActive(false);
-            Respawn();
+            if (currentHealth == 3)
+            {
+                heart4.SetActive(false);
+            }
+            else if (currentHealth == 2)
+            {
+                heart3.SetActive(false);
+            }
+            else if (currentHealth == 1)
+            {
+                heart2.SetActive(false);
+            }
+            else if (currentHealth == 0)
+            {
+                heart1.SetActive(false);
+                Respawn();
+            }
         }
     }
 
     void HealthIncrease()
     {
         currentHealth = currentHealth + 1;
+        if (AnotherHeartIsBought == false)
+        {
+            if (currentHealth == 4)
+            {
+                Debug.Log("Current health is greater than maximum health!");
+            }
+            if (currentHealth == 3)
+            {
+                heart3.SetActive(true);
+            }
+            else if (currentHealth == 2)
+            {
+                heart2.SetActive(true);
+            }
+        }
 
-        if (currentHealth == 4)
+        if (AnotherHeartIsBought)
         {
-            Debug.Log("Current health is greater than maximum health!");
-        }
-        if (currentHealth == 3)
-        {
-            heart3.SetActive(true);
-        }
-        else if (currentHealth == 2)
-        {
-            heart2.SetActive(true);
+            if (currentHealth == 4)
+            {
+                heart4.SetActive(true);
+            }
+            if (currentHealth == 3)
+            {
+                heart3.SetActive(true);
+            }
+            else if (currentHealth == 2)
+            {
+                heart2.SetActive(true);
+            }
         }
     }
 
     void Update()
     {
-        if (is_on_Ground && Time.time - collision_lasting >= allowed_collision_lasting)
+
+        if ((is_on_Ground && Time.time - collision_lasting >= allowed_collision_lasting) || (is_touching_enemy && Time.time - collision_lasting >= allowed_collision_lasting))
         {
             HealthDecrease();
             collision_lasting = Time.time; 
         }
+
+        int my = PlayerPrefs.GetInt("AnotherHeartIsBought", ValueOfHeart);
+        AnotherHeartIsBought = my == ValueOfHeart;
+
+         if (my == 0)
+          {
+             heart4.SetActive(true);
+             maxHealth = 4;
+          }
+        
+
     }
 
     void Respawn()
@@ -119,6 +193,14 @@ public class Health : MonoBehaviour
     
     }
 
+    public void AddHeartWasBought()
+    {
+        AnotherHeartIsBought = true;
+
+        PlayerPrefs.SetInt("AnotherHeartIsBought", 0);
+
+        PlayerPrefs.Save();
+    }
+
 }
-
-
+        
